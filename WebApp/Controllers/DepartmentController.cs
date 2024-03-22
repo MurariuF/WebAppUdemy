128193acb33556e2ddde4e3a5220790248fb4466
MurariuF <murariu.flavius@gmail.com>
2024-03-22 14:52:48 +0200

    Added controller for Department

---
+++ WebApp/Controllers/DepartmentController.cs
@@ -0,0 +1,142 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = "select DepartmentId,DepartmentName from mytestdb.Department";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = configuration.GetConnectionString("EmployeeAppCon");
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department department)
+        {
+            string query = @"insert into mytestdb.Department (DepartmentName) values (@DepartmentName)";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = configuration.GetConnectionString("EmployeeAppCon");
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department department)
+        {
+            string query = @"update mytestdb.Department set
+                            DepartmentName = @DepartmentName
+                            where DepartmentId=@DepartmentId";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = configuration.GetConnectionString("EmployeeAppCon");
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                    myCommand.Parameters.AddWithValue("@DepartmentId", department.DepartmentId);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Update Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Post(int id)
+        {
+            string query = @"delete from mytestdb.Department
+                             where DepartmentId=@DepartmentId";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = configuration.GetConnectionString("EmployeeAppCon");
+
+            MySqlDataReader myReader;
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
